08d470f0857fe92603b0d0ebce7c28e266910cf6
ccc94633-star <ccc94633@gmail.com>
2026-06-08 23:06:27 +0800

    ass action file

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/portfolio-chiikawa-generator-web/',
+  base: '/portfolio-web/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
