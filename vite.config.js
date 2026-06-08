f3df6ce6cd945abc37203295d17e1a8a98f95fbb
ccc94633-star <ccc94633@gmail.com>
2026-06-08 23:12:10 +0800

    修正vite

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/portfolio-web/',
+  base: '/portfolios-web/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
